06e2c393a20bc66d3cd5766fd35ed92d03119d16
unknownhero <unknownpostie@gmail.com>
2025-06-26 18:33:36 +0300

    feat(ruby): variables
    variables, const and internal ruby const example

---
+++ lang/bhv-book/3-variables/main.rb
@@ -0,0 +1,11 @@
+sum_result = 2 + 2
+puts "sum_result: #{sum_result}"
+
+CONST = 12
+puts "CONST: #{CONST}"
+
+# warning, not error. value will be redefined
+#CONST = 13
+
+puts  "Ruby version: #{RUBY_VERSION}"
+

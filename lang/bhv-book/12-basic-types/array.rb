d47c93796907340544b24835265301dc7df4ed8e
unknownhero <unknownpostie@gmail.com>
2025-06-27 15:35:47 +0300

    feat(ruby): array examples

---
+++ lang/bhv-book/12-basic-types/array.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+p [1, 2, 3, 4, 5]
+arr = [1, 2, 3, 4, 5]
+p arr[0] # 1
+p arr[-1] # 5
+p arr[2, 2] # [3, 4]
+p arr[2..3] # [3, 4]
+p [1, 'hello', 3, ['first', 2]]
+
+colors = %w[blue red white yellow]
+p colors
+
+colors = %i[red orange yellow green blue indigo violet]
+p colors

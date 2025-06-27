aa769cfdd0d7cd2f498b314f3e467fe90e2ac8b7
unknownhero <unknownpostie@gmail.com>
2025-06-27 08:47:37 +0300

    feat(ruby): methods basics

---
+++ lang/bhv-book/6-methods/main.rb
@@ -0,0 +1,17 @@
+
+# function example
+def greeting
+  puts "Hello, function!"
+end
+
+greeting
+
+# class method example
+class Hello
+  def greeting
+    puts "Hello, method!"
+  end
+end
+
+hello = Hello.new
+hello.greeting

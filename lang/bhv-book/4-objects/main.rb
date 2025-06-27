6dc3f75ec5ebaaa0624debaa78ca417de321b2e2
unknownhero <unknownpostie@gmail.com>
2025-06-27 08:16:18 +0300

    feat(ruby): objects basics

---
+++ lang/bhv-book/4-objects/main.rb
@@ -0,0 +1,15 @@
+# string as object
+string_object_id = 'Hello world!'.object_id;
+puts "String object id: #{string_object_id}"
+
+# number method example
+is_number_between = 3.between? 0, 10;
+puts "Is number between: #{is_number_between}"
+
+# boolean as object
+true_object_id = true.object_id
+puts "True object_id: #{true_object_id}"
+
+# stdout example
+stdout.puts 'puts as part of stdout'
+

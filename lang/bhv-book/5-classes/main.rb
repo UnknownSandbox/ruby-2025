6a88c132f5d1baf28a7099d538e5ede93fa06d11
unknownhero <unknownpostie@gmail.com>
2025-06-28 18:13:36 +0300

    feat(ruby): classes update

--- lang/bhv-book/5-classes/main.rb
+++ lang/bhv-book/5-classes/main.rb
@@ -9,4 +9,86 @@ end
 hello_object = Hello.new
 puts "Class name of hello_object: #{hello_object.class}"
 
+# wat ????
+class HelloWorld
+  puts 'start' # work like static constructor
+  puts 'end'
+end
 
+hello = HelloWorld.new
+p hello
+
+# car
+class Car
+  def title
+    'BMW X7'
+  end
+
+  def description
+    'b m w'
+  end
+
+  attr_reader :engine
+
+  def build
+    @engine = Engine.new
+  end
+
+  # engine
+  class Engine
+    def cylinders
+      6
+    end
+
+    def volume
+      3
+    end
+
+    def power
+      250
+    end
+  end
+end
+
+car = Car.new
+car.build
+puts car.title # BMW X7
+puts car.description
+puts car.engine.cylinders # 6
+puts car.engine.volume # 3
+puts car.engine.power # 250
+
+engine = Car::Engine.new
+
+puts engine.cylinders # 6
+puts engine.volume # 3
+puts engine.power # 250
+
+# class with constructor
+class Ticket
+  def initialize
+    @price = 500
+  end
+
+  attr_reader :price
+end
+
+ticket = Ticket.new
+puts ticket.price
+
+# ticket with constructor with params
+class Ticket2
+  def initialize(price)
+    @price = price
+  end
+
+  attr_reader :price
+end
+
+ticket = Ticket2.new 800
+puts ticket.price
+puts ticket
+
+ticket = Ticket.allocate
+puts ticket.price.nil?
+puts ticket
